f116f455fd4a4c16d33c9a63fa7385e33c72fa1d
Yevhen Radionov <eugene.radionov@gmail.com>
2017-12-05 17:10:12 +0200

    Fixes.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  self.record_timestamps = true
   belongs_to :task, counter_cache: true
   validates :body, length: { in: 10..256 }, presence: true
   mount_base64_uploader :attachment, AttachmentUploader
