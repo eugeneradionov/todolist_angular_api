2e2f62eb79eb6a0874ace270d6a23e8a1d657625
Yevhen Radionov <eugene.radionov@gmail.com>
2017-12-05 17:23:44 +0200

    Added CanCan Ability.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, :all
+    unless user.new_record?
+      can :manage, Project, user_id: user.id
+      can :manage, Task, project: { user_id: user.id }
+      can :manage, Comment, task: { project: { user_id: user.id } }
+    end
   end
 end
